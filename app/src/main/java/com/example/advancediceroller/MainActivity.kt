3767160b6a2dd3ccb6c29b398b88aa7215dbba59
Festus Omole <festus14@fb.com>
2023-03-15 22:06:34 +0000

    fixed navGraph issue by AppCompatActivity in MainActivity

--- app/src/main/java/com/example/advancediceroller/MainActivity.kt
+++ app/src/main/java/com/example/advancediceroller/MainActivity.kt
@@ -1,15 +1,16 @@
-package com.example.advancediceroller;
+package com.example.advancediceroller
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
-import androidx.annotation.Nullable;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
