85205fb57a755dff58b3af411797783ec859286a
Festus Omole <festus14@fb.com>
2023-03-26 23:10:45 +0100

    Added onclick listener to sign up button amd designed sign up page

---
+++ app/src/main/java/com/example/advancediceroller/home/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.example.advancediceroller.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.advancediceroller.R
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+         val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val signUpBtn = view.findViewById<Button>(R.id.signUpBtn)
+        val signInBtn = view.findViewById<Button>(R.id.signInBtn)
+
+        signUpBtn.setOnClickListener{
+            val action =
+            HomeFragmentDirections.actionHomeFragmentToSignUpFragment()
+            findNavController().navigate(action)
+        }
+
+        signInBtn.setOnClickListener{
+            val action =
+            HomeFragmentDirections.actionHomeFragmentToSignInFragment()
+            findNavController().navigate(action)
+        }
+
+        return view
+    }
+}
